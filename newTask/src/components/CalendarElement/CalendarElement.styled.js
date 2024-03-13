4dc9ad96f53b9e35c9ed534c9c4168eab9cd2536
Vlad <vzhalin@bk.ru>
2024-03-13 14:01:22 +0300

    finish

---
+++ newTask/src/components/CalendarElement/CalendarElement.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const UnderText = styled.p`
+  margin-top: 14px;
+  font-family: Roboto;
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 12px;
+  letter-spacing: 0em;
+  margin-left: 11px;
+`;
+
+export const DateTitle = styled.p`
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 16px;
+  letter-spacing: 0px;
+  text-align: left;
+  margin-left: 16.5px;
+`;
